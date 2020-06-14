a9ed0b1ce6365de92df392ad52e1d50dda42f1ed
glbian <glbian@hotmail.com>
2020-06-14 10:12:23 +0800

    leetcode project

--- leetcode/64_interview_sum_1_n.cpp
+++ leetcode/64_interview_sum_1_n.cpp
@@ -1,3 +1,17 @@
+
+class Solution {
+public:
+    int sumNums(int n) {
+        int sum = 0;
+
+        /* 递归算法 - 利用逻辑与&&的阻断性质终止递归 */
+        n && (sum = (sumNums(n - 1) + n));
+
+        return sum;
+    }
+};
+
+
 class Solution {
 public:
     int sumNums(int n) {
